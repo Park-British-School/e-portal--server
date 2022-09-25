01a1f0fab7964b04497b7191e703b6bde96d35ad
ilechukwu joshua <ilechuks73@gmail.com>
2022-09-26 00:11:24 +0100

    added find all and count all endpoints

--- routers/result.js
+++ routers/result.js
@@ -10,11 +10,45 @@ const {
   downloadResult,
 
   countAllResults,
+  findAllResults,
 } = require("../controllers/resultController");
 const aggregateScores = require("../middlewares/aggregateScores");
 const generateResultPDF = require("../middlewares/generateResultPDF");
 
+// TO BE REMOVED AFTER STABLE
 router.get("/", getAllResults);
+// END
+
+router.get("/find-all", (request, response) => {
+  findAllResults((error, results) => {
+    if (error) {
+      response.status(400).send(error);
+    } else {
+      response.status(200).json(results);
+    }
+  });
+});
+
+router.get("/find", (request, response) => {
+  findAllResults((error, results) => {
+    if (error) {
+      response.status(400).send(error);
+    } else {
+      response.status(200).json(results);
+    }
+  });
+});
+
+router.get("/count-all", (request, response) => {
+  countAllResults((error, count) => {
+    if (error) {
+      response.status(400).send(error);
+    } else {
+      response.status(200).send(count.toString());
+    }
+  });
+});
+
 router.get("/:id", getResult);
 router.get("/class/:classID", getResultsByClass);
 router.post("/", aggregateScores, addResult);
@@ -22,15 +56,5 @@ router.post("/:resultID/edit", editResult);
 router.get("/:resultID/approve", approveResult);
 router.get("/:resultID/download", downloadResult, generateResultPDF);
 router.get("/delete/:resultID", deleteResult);
-
-router.get("/count-all", (request, response) => {
-  countAllResults((error, count) => {
-    if (error) {
-      response.status(400).send(error);
-    } else {
-      response.status(200).send(count);
-    }
-  });
-});
 
 module.exports = router;
