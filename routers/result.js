a924780b9b22d23bd23dd1344de80075da87c330
ilechukwu joshua <ilechuks73@gmail.com>
2022-09-25 09:00:27 +0100

    added count enpoint

--- routers/result.js
+++ routers/result.js
@@ -1,4 +1,4 @@
-const router = require('express').Router()
+const router = require("express").Router();
 const {
   getAllResults,
   getResult,
@@ -7,19 +7,30 @@ const {
   editResult,
   approveResult,
   getResultsByClass,
-  downloadResult
-} = require("../controllers/resultController")
-const aggregateScores = require('../middlewares/aggregateScores')
-const generateResultPDF = require('../middlewares/generateResultPDF')
+  downloadResult,
 
-router.get("/", getAllResults)
-router.get("/:id", getResult)
-router.get("/class/:classID", getResultsByClass)
-router.post("/", aggregateScores, addResult)
-router.post("/:resultID/edit", editResult)
-router.get("/:resultID/approve", approveResult)
-router.get("/:resultID/download", downloadResult, generateResultPDF)
-router.get("/delete/:resultID", deleteResult)
+  countAllResults,
+} = require("../controllers/resultController");
+const aggregateScores = require("../middlewares/aggregateScores");
+const generateResultPDF = require("../middlewares/generateResultPDF");
 
+router.get("/", getAllResults);
+router.get("/:id", getResult);
+router.get("/class/:classID", getResultsByClass);
+router.post("/", aggregateScores, addResult);
+router.post("/:resultID/edit", editResult);
+router.get("/:resultID/approve", approveResult);
+router.get("/:resultID/download", downloadResult, generateResultPDF);
+router.get("/delete/:resultID", deleteResult);
 
-module.exports = router
+router.get("/count-all", (request, response) => {
+  countAllResults((error, count) => {
+    if (error) {
+      response.status(400).send(error);
+    } else {
+      response.status(200).send(count);
+    }
+  });
+});
+
+module.exports = router;
