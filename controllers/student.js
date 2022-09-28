877fd9174820e8ed94c996c117b98262b606f162
ilechukwu joshua <ilechuks73@gmail.com>
2022-09-28 01:45:48 +0100

    added find student by id and find student by name

--- controllers/student.js
+++ controllers/student.js
@@ -1,26 +1,29 @@
-const mongoose = require("mongoose")
-const models = require('../models')
-const bcrypt = require("bcrypt")
-const fs = require("fs")
-const path = require('path')
-const jsonwebtoken = require("jsonwebtoken")
+const mongoose = require("mongoose");
+const models = require("../models");
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const path = require("path");
+const jsonwebtoken = require("jsonwebtoken");
 
-const Student = require("../models/studentModel")
-const Class = require("../models/classModel")
-const Result = require("../models/resultModel")
+const Student = require("../models/studentModel");
+const Class = require("../models/classModel");
+const Result = require("../models/resultModel");
 
-const { notificationModel } = models
+const { notificationModel } = models;
 
-exports.getAllStudents = async function (req, res) {
-  const student = await Student.find({}).populate('class')
-  res.json(student)
-}
+exports.findAllStudents = async function (req, res) {
+  const student = await Student.find({}).populate("class");
+  res.json(student);
+};
 
 exports.getStudent = async function (req, res) {
-  const studentID = req.params.studentID.replace(/-/g, "/")
-  const student = await Student.findById(studentID).populate(['class', "results"])
-  res.status(200).json(student)
-}
+  const studentID = req.params.studentID.replace(/-/g, "/");
+  const student = await Student.findById(studentID).populate([
+    "class",
+    "results",
+  ]);
+  res.status(200).json(student);
+};
 
 exports.addStudent = async function (req, res) {
   try {
@@ -34,47 +37,50 @@ exports.addStudent = async function (req, res) {
       phoneNumber: req.body.phoneNumber,
       gender: req.body.gender,
       address: req.body.address,
-      password: hashedPassword
-    }).save()
+      password: hashedPassword,
+    }).save();
     if (req.body.class) {
-      const isValid = mongoose.Types.ObjectId.isValid(req.body.class)
+      const isValid = mongoose.Types.ObjectId.isValid(req.body.class);
       if (isValid) {
         await Class.updateOne(
           { _id: req.body.class },
           { $push: { students: newStudent._id } }
-        )
+        );
       } else {
-        throw "This class doesnt exist"
+        throw "This class doesnt exist";
       }
     }
     if (req.body.image) {
       fs.writeFile(
-        `${__dirname}/../uploads/images/profile/${newStudent._id.toString().replace(/\//g, "-")}.jpg`,
+        `${__dirname}/../uploads/images/profile/${newStudent._id
+          .toString()
+          .replace(/\//g, "-")}.jpg`,
         req.body.image,
-        'base64',
+        "base64",
         (err) => {
           if (err) {
-            throw "upload rror"
+            throw "upload rror";
           }
         }
-      )
+      );
     }
-    res.status(200).json(newStudent)
-  }
-  catch (err) {
+    res.status(200).json(newStudent);
+  } catch (err) {
     res.status(400).json({
       error: {
-        message: err
-      }
-    })
+        message: err,
+      },
+    });
   }
-}
+};
 
 exports.login = async function (req, res) {
-
   try {
     //CHECK IF THE STUDENT EXISTS
-    const student = await Student.findOne({ _id: req.body.id }).populate(["class", "results"])
+    const student = await Student.findOne({ _id: req.body.id }).populate([
+      "class",
+      "results",
+    ]);
     if (student) {
       //CHECK IF THE PASSWORD MATCHES
       const isPasswordMatched = await bcrypt.compare(
@@ -88,157 +94,184 @@ exports.login = async function (req, res) {
           process.env.TOKEN_SECRET
         );
         //CHECK IF THE STUDENT ACCOUNT IS ACTIVE
-        const isActive = student.status === "active"
+        const isActive = student.status === "active";
         if (isActive) {
-          await Student.updateOne({ _id: student._id }, { lastSeen: new Date().getTime() })
+          await Student.updateOne(
+            { _id: student._id },
+            { lastSeen: new Date().getTime() }
+          );
           res.status(200).json({ authToken: token, ...student._doc });
+        } else {
+          throw "This account has been deactivated, Please contact an administrator";
         }
-        else {
-          throw "This account has been deactivated, Please contact an administrator"
-        }
+      } else {
+        throw "Invalid ID or Password";
       }
-      else {
-        throw "Invalid ID or Password"
-      }
+    } else {
+      throw "Invalid ID or Password";
     }
-    else {
-      throw "Invalid ID or Password"
-    }
-
-
-  }
-  catch (err) {
-    console.log(err)
+  } catch (err) {
+    console.log(err);
     res.status(400).json({
       error: {
-        message: err
-      }
-    })
+        message: err,
+      },
+    });
   }
-}
+};
 
 exports.activate = async function (req, res) {
   //RE-FORMAT THE STUDENT ID
-  const studentID = req.params.studentID.replace(/-/g, "/")
+  const studentID = req.params.studentID.replace(/-/g, "/");
   try {
     //CHECK IF THE ACCOUNT EXISTS
-    const student = await Student.findById(studentID)
+    const student = await Student.findById(studentID);
     if (student) {
       if (student.status === "inactive") {
-        await Student.updateOne({ _id: studentID }, { status: "active" })
-        res.status(200).json({})
+        await Student.updateOne({ _id: studentID }, { status: "active" });
+        res.status(200).json({});
+      } else {
+        throw "Account already activated";
       }
-      else {
-        throw "Account already activated"
-      }
+    } else {
+      throw "This account doesn't exist, please try again";
     }
-    else {
-      throw "This account doesn't exist, please try again"
-    }
-
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).json({
       error: {
-        message: error
-      }
-    })
+        message: error,
+      },
+    });
   }
-}
+};
 
 exports.deactivate = async function (req, res) {
   //RE-FORMAT THE STUDENT ID
-  const studentID = req.params.studentID.replace(/-/g, "/")
+  const studentID = req.params.studentID.replace(/-/g, "/");
   try {
     //CHECK IF THE ACCOUNT EXISTS
-    const student = await Student.findById(studentID)
+    const student = await Student.findById(studentID);
     if (student) {
       if (student.status === "active") {
-        await Student.updateOne({ _id: studentID }, { status: "inactive" })
-        res.status(200).json({})
+        await Student.updateOne({ _id: studentID }, { status: "inactive" });
+        res.status(200).json({});
+      } else {
+        throw "Account already deactivated";
       }
-      else {
-        throw "Account already deactivated"
-      }
+    } else {
+      throw "This account doesn't exist, please try again";
     }
-    else {
-      throw "This account doesn't exist, please try again"
-    }
-
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).json({
       error: {
-        message: error
-      }
-    })
+        message: error,
+      },
+    });
   }
-}
+};
 
 exports._delete = async function (req, res) {
   try {
     //RE-FORMAT THE STUDENT ID
-    const studentID = req.params.studentID.replace(/-/g, "/")
-    const student = await Student.findByIdAndDelete(studentID)
+    const studentID = req.params.studentID.replace(/-/g, "/");
+    const student = await Student.findByIdAndDelete(studentID);
     await Class.updateOne(
       { _id: student.class },
       { $pull: { students: student._id } }
-    )
+    );
     await student.results.forEach(async (result) => {
-      await Result.deleteOne({ _id: result })
-    })
-    res.status(200).json({})
+      await Result.deleteOne({ _id: result });
+    });
+    res.status(200).json({});
   } catch (err) {
     res.status(400).json({
       error: {
-        message: err
-      }
-    })
+        message: err,
+      },
+    });
   }
-
-}
+};
 
 exports.editProfile = async (req, res) => {
   try {
-    const studentID = req.params.studentID.replace(/-/g, "/")
-    await Student.updateOne(
-      { _id: studentID },
-      { ...req.body }
-    )
-    res.status(200).json({})
+    const studentID = req.params.studentID.replace(/-/g, "/");
+    await Student.updateOne({ _id: studentID }, { ...req.body });
+    res.status(200).json({});
   } catch (error) {
     res.status(400).json({
       error: {
-        message: error
-      }
-    })
+        message: error,
+      },
+    });
   }
-}
+};
 
 exports.getStudents = async function (req, res) {
   try {
-    const studentID = req.body.studentID
-    const students = req.body.find({ id: studentID }).populate(['class', "results"])
-
+    const studentID = req.body.studentID;
+    const students = req.body
+      .find({ id: studentID })
+      .populate(["class", "results"]);
   } catch (error) {
     res.status(400).json({
       error: {
-        message: error
-      }
-    })
+        message: error,
+      },
+    });
   }
-}
+};
 
 exports.getAllNotifications = async function (request, response) {
-  const studentID = request.params.studentID.replace(/-/g, "/")
-  notificationModel().find(
-    { recipient: studentID },
-    (error, documents) => {
-      response.status(200).json(documents)
+  const studentID = request.params.studentID.replace(/-/g, "/");
+  notificationModel().find({ recipient: studentID }, (error, documents) => {
+    response.status(200).json(documents);
+  });
+};
+
+exports.deleteNotificationByID = async function (request, response) {};
+
+// REFACTORING STARTS HERE
+
+exports.countAllStudents = async function (callback) {
+  Student.countDocuments({}, (error, count) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, count);
     }
-  )
+  });
+};
+
+exports.findAllStudents = async function (callback) {
+  Student.findAll((error, document) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, document);
+    }
+  });
+};
+
+exports.findStudentByID = async function (studentID, callback) {
+  Student.findByID(studentID, (error, document)=>{
+    if(error){
+      callback(error, null)
+    }
+    else{
+      callback(null, document)
+    }
+  })
+};
+
+exports.findStudentByName = async function (name, callback){
+  Student.findByName(name, (error, document)=>{
+    if(error){
+      callback(error, null)
+    }
+    else{
+      callback(null, document)
+    }
+  })
 }
 
-exports.deleteNotificationByID = async function (request, response) {
-  
-}
+//REFACTORING ENDS HERE
