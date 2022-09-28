74817627b29844dc4feb02b546641e1ab57293bd
ilechukwu joshua <ilechuks73@gmail.com>
2022-09-28 01:55:33 +0100

    added count all route added find all router refactoring

--- routers/class.js
+++ routers/class.js
@@ -1,15 +1,41 @@
-const router = require('express').Router()
-const { getAllClasses, getClass, addClass, assignStudent, assignTeacher, addSubject, deleteSubject, getAllSubjects, removeStudent, removeTeacher } = require('../controllers/classController')
+const router = require("express").Router();
+const controllers = require("../controllers");
 
-router.get("/", getAllClasses)
-router.get("/:classID", getClass)
-router.post("/addClass", addClass)
-router.post("/assignstudent", assignStudent)
-router.post("/:classID/students/remove", removeStudent)
-router.post("/assignteacher", assignTeacher)
-router.post("/:classID/teachers/remove", removeTeacher)
-router.post("/:classID/subjects/", getAllSubjects)
-router.post("/:classID/subjects/add", addSubject)
-router.post("/:classID/subjects/delete", deleteSubject)
+const { classController } = controllers;
 
-module.exports = router
+// router.get("/", getAllClasses)
+// router.get("/:classID", getClass)
+// router.post("/addClass", addClass)
+// router.post("/assignstudent", assignStudent)
+// router.post("/:classID/students/remove", removeStudent)
+// router.post("/assignteacher", assignTeacher)
+// router.post("/:classID/teachers/remove", removeTeacher)
+// router.post("/:classID/subjects/", getAllSubjects)
+// router.post("/:classID/subjects/add", addSubject)
+// router.post("/:classID/subjects/delete", deleteSubject)
+
+// REFACTORING STARTS HERE
+
+router.get("/count-all", (request, response) => {
+  classController.countAllClasses((error, count) => {
+    if (error) {
+      response.status(400).send(error);
+    } else {
+      response.status(200).send(count.toString());
+    }
+  });
+});
+
+router.get("/find-all", (request, response) => {
+  classController.findAllClasses((error, classes) => {
+    if (error) {
+      response.status(400).send(error);
+    } else {
+      response.status(200).json(classes);
+    }
+  });
+});
+
+//REFACTORING ENDS HERE
+
+module.exports = router;
