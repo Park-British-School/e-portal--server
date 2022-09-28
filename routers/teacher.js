16e672ab9b5ec0671b9a175e0a85b66fff577422
ilechukwu joshua <ilechuks73@gmail.com>
2022-09-28 01:58:37 +0100

    added count all route added find all route added find one route

--- routers/teacher.js
+++ routers/teacher.js
@@ -1,22 +1,85 @@
-const router = require('express').Router()
-const {
-  getAllTeachers,
-  getTeacher,
-  addTeacher,
-  login,
-  activate,
-  deactivate,
-  _delete,
-  editProfile
-} = require("../controllers/teacherController")
+const router = require("express").Router();
+const controllers = require("../controllers");
 
-router.get("/", getAllTeachers)
-router.get("/:id", getTeacher)
-router.post("/addTeacher", addTeacher)
-router.post("/signin", login)
-router.post("/:teacherID/profile/edit", editProfile)
-router.get("/activate/:teacherID", activate)
-router.get("/deactivate/:teacherID", deactivate)
-router.get("/delete/:teacherID", _delete)
+const { teacherController } = controllers;
 
-module.exports = router
+// router.get("/", getAllTeachers)
+// router.get("/:id", getTeacher)
+// router.post("/addTeacher", addTeacher)
+// router.post("/signin", login)
+// router.post("/:teacherID/profile/edit", editProfile)
+// router.get("/activate/:teacherID", activate)
+// router.get("/deactivate/:teacherID", deactivate)
+// router.get("/delete/:teacherID", _delete)
+
+// REFACTORING STARTS HERE
+
+router.get("/count-all", (request, response) => {
+  teacherController.countAllTeachers((error, count) => {
+    if (error) {
+      response.status(400).send(error);
+    } else {
+      response.status(200).send(count.toString());
+    }
+  });
+});
+
+router.get("/find-all", (request, response) => {
+  teacherController.findAllTeachers((error, teachers) => {
+    if (error) {
+      response.status(400).send(error);
+    } else {
+      response.status(200).json(teachers);
+    }
+  });
+});
+
+router.get("/find-one", (request, response) => {
+  if (request.query.by) {
+    switch (request.query.by) {
+      case "ID":
+        teacherController.findTeacherByID(
+          request.query.ID,
+          (error, teacher) => {
+            if (error) {
+              response.status(400).send(error);
+            } else {
+              if (teacher) {
+                response.status(200).json(teacher);
+              } else {
+                response.status(400).send(`teacher does not exist`);
+              }
+            }
+          }
+        );
+        break;
+
+      case "name":
+        teacherController.findTeacherByName(
+          request.query.name,
+          (error, teacher) => {
+            if (error) {
+              response.status(400).send(error);
+            } else {
+              if (teacher) {
+                response.status(200).json(teacher);
+              } else {
+                response.status(400).send(`teacher does not exist`);
+              }
+            }
+          }
+        );
+        break;
+
+      default:
+        response.status(400).send("Incorrect query parameters");
+        break;
+    }
+  } else {
+    response.status(400).send("Incorrect query parameters");
+  }
+});
+
+//REFACTORING ENDS HERE
+
+module.exports = router;
