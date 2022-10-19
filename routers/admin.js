a22470b559ebd836734158829db7c3ef89844f85
Ilechukwu Joshua <ilechuks73@gmail.com>
2022-10-19 18:24:52 +0100

    -added "/find-all" route -removed deprecated routes -added "/create" route

--- routers/admin.js
+++ routers/admin.js
@@ -1,11 +1,49 @@
-const router = require("express").Router();
+const express = require("express");
 const controllers = require("../controllers");
 
 const { adminController } = controllers;
+const { Router } = express;
 
-// router.get("/", getAllAdmins);
-// router.get("/:id", getAdmin);
-// router.post("/addAdmin", addAdmin);
-// router.post("/signin", login);
+const adminRouter = Router();
 
-module.exports = router;
+adminRouter.get("/find-all", (request, response) => {
+  adminController.findAllAdmins(
+    {
+      paginate: request.query.paginate === "true" ? true : false,
+      count: request.query.count ? parseInt(request.query.count) : 10,
+      page: request.query.page ? parseInt(request.query.page) : 1,
+    },
+    (error, admins) => {
+      if (error) {
+        response.status(400).send(error);
+      } else {
+        response.status(200).send(admins);
+      }
+    }
+  );
+});
+
+adminRouter.post("/create", (request, response) => {
+  adminController.findAdminByEmailAddress(
+    request.body.emailAddress,
+    (error, document) => {
+      if (error) {
+        response.status(400).send(error);
+      } else {
+        if (document) {
+          response.status(400).send("This email already exists");
+        } else {
+          adminController.createAdmin({ ...request.body }, (error, admin) => {
+            if (error) {
+              response.status(400).send(error);
+            } else {
+              response.status(200).json(admin);
+            }
+          });
+        }
+      }
+    }
+  );
+});
+
+module.exports = adminRouter;
