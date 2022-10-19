15cf4794bbba122b6f5374f15d73d169c85a1a65
Ilechukwu Joshua <ilechuks73@gmail.com>
2022-10-19 18:19:54 +0100

    refactoring

--- controllers/term.js
+++ controllers/term.js
@@ -1,17 +1,16 @@
-const Term = require("../models/termModel")
+const Term = require("../models/termModel");
 
 exports.getTerm = async function (req, res) {
-  const data = await Term.findOne({})
+  const data = await Term.findOne({});
   if (!data) {
-    await new Term({name: "Not Set"}).save()
-    res.status(200).json({ term: "Not Set" })
+    await new Term({ name: "Not Set" }).save();
+    res.status(200).json({ term: "Not Set" });
+  } else {
+    res.status(200).json({ term: data.name });
   }
-  else {
-    res.status(200).json({ term: data.name })
-  }
+};
 
-}
 exports.setTerm = async function (req, res) {
-  await Term.updateOne({}, { name: req.body.term })
-  res.status(200).send({}) 
-}
+  await Term.updateOne({}, { name: req.body.term });
+  res.status(200).send({});
+};
