645e0bc2ba97e097ab8b6b65fce725ca9ed5e5d5
Ilechukwu Joshua <ilechuks73@gmail.com>
2022-10-19 18:26:20 +0100

    -added pagination to "/find-all" route -added "/find-one" route

--- routers/class.js
+++ routers/class.js
@@ -23,13 +23,60 @@ router.get("/count-all", (request, response) => {
 });
 
 router.get("/find-all", (request, response) => {
-  classController.findAllClasses((error, classes) => {
-    if (error) {
-      response.status(400).send(error);
-    } else {
-      response.status(200).json(classes);
+  classController.findAllClasses(
+    {
+      paginate: request.query.paginate === "true" ? true : false,
+      count: request.query.count ? parseInt(request.query.count) : 10,
+      page: request.query.page ? parseInt(request.query.page) : 1,
+    },
+    (error, classes) => {
+      if (error) {
+        response.status(400).send(error);
+      } else {
+        response.status(200).json(classes);
+      }
     }
-  });
+  );
+});
+
+router.get("/find-one", (request, response) => {
+  if (request.query.by) {
+    switch (request.query.by) {
+      case "ID":
+        classController.findClassByID(request.query.ID, (error, _class) => {
+          if (error) {
+            response.status(400).send(error);
+          } else {
+            if (_class) {
+              response.status(200).json(_class);
+            } else {
+              response.status(400).send(`class does not exist`);
+            }
+          }
+        });
+        break;
+
+      case "name":
+        classController.findClassByName(request.query.name, (error, _class) => {
+          if (error) {
+            response.status(400).send(error);
+          } else {
+            if (_class) {
+              response.status(200).json(_class);
+            } else {
+              response.status(400).send(`class does not exist`);
+            }
+          }
+        });
+        break;
+
+      default:
+        response.status(400).send("Incorrect query parameters");
+        break;
+    }
+  } else {
+    response.status(400).send("Incorrect query parameters");
+  }
 });
 
 router.get("/:classID", classController.getClass);
